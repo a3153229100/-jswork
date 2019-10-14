f8225c1cba313a40de6f48f3f64dc277b9f86b36
apple <apple@jd505-03.local>
2019-10-14 15:27:08 +0800

    work5

---
+++ jswork/jswork5/work.js
@@ -0,0 +1,25 @@
+//9*9的空表，请自行编写这段代码
+str = '<table border="1">'
+for (let i=9;i>0;--i){
+    str += '<tr>'
+    for (var j=9;j>0;--j){
+        str +='<td>&nbsp;</td>'
+    }
+    str +='</tr>'       
+ }
+    str +='</tr>'   
+document.getElementById('table1').innerHTML = str
+
+//倒九九乘法空表
+str = '<table border="1">'
+for (let i=9;i>0;--i){
+    str += '<tr>'
+    for (var j=i;j>0;--j){
+        str +='<td>&nbsp;</td>'
+    }
+    str +='</tr>'       
+ }
+    str +='</tr>'   
+document.getElementById('table2').innerHTML = str
+
+倒九九乘法表请自行编写代码
