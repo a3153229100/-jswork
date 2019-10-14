543aedd5eb5058b3eaab1378d508d8e6376b4c38
apple <apple@jd505-03.local>
2019-10-14 15:58:04 +0800

    work5

--- jswork/jswork5/work.js
+++ jswork/jswork5/work.js
@@ -22,4 +22,18 @@ for (let i=9;i>0;--i){
     str +='</tr>'   
 document.getElementById('table2').innerHTML = str
 
-倒九九乘法表请自行编写代码
+
+//倒九九乘法表请自行编写代码
+str = '<table border="1">'
+for (let i=9;i>0;--i){
+    str += '</tr>'
+    for (let j=i;j>0;--j){
+       str +='<td>&nbsp;'+i+'x'+j+'='+(i*j)+'</td>'      
+       } 
+       str += '</tr>'
+    }
+    str +='</table>'
+    document.getElementById('table3').innerHTML = str
+        
+   
+
